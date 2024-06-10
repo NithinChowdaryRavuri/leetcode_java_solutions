a0ed6ef4399f5bd279914d2b791da428f0e94cb7
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-06-10 07:48:02 -0500

    Added 1051 solution

---
+++ 1051/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] ans = new int[heights.length];
+        for (int i = 0; i < heights.length; i++) {
+            ans[i] = heights[i];
+        }
+        Arrays.sort(ans);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (ans[i] != heights[i]) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
