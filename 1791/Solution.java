9fb3b6312dacf885f6467358d442c421ffbdc00b
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-06-26 19:30:01 -0500

    Added 1791 solution

---
+++ 1791/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        int a = edges[0][0], b = edges[0][1];
+        if (edges[1][0] == a || edges[1][1] == a)
+            return a;
+        return b;
+    }
+}
