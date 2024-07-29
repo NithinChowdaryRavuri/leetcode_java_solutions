2dd1925100ee5d16dad1e66b5d7f00ac2db43a85
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-07-29 14:07:40 -0500

    Added 1395 solution

---
+++ 1395/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        int n = rating.length;
+        int result = 0;
+
+        for(int i=0; i<n-2; i++) {
+            for (int j=i+1; j<n-1; j++) {
+                if(rating[i] > rating[j]) continue;
+                for (int k = j+1; k<n; k++) {
+                    if (rating[j] < rating[k] ) {
+                        result += 1;
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<n-2; i++) {
+            for (int j=i+1; j<n-1; j++) {
+                if(rating[i] < rating[j]) continue;
+                for (int k = j+1; k<n; k++) {
+                    if (rating[j] > rating[k] ) {
+                        result += 1;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
