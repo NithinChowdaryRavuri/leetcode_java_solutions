6861c2b93c52a0b65c975772fa75b30d2eb8a28a
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-08-05 07:13:31 -0500

    Added 2053 solution

---
+++ 2053/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word: arr) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        for (String word: arr) {
+            if (map.get(word) == 1) {
+                k--;
+                if (k==0) return word;
+            }
+        }
+
+        return "";
+    }
+}
